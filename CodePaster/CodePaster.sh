6a0d4730691b694216622381c048ca9f244d08c8
JonMS95 <jon_portugalete@hotmail.com>
2022-11-13 19:09:52 +0100

    Added a bit more of complexity to the source file. Codepaster.sh file now removes both the created source file after compiling and the compiled executable file after running it.

--- CodePaster/CodePaster.sh
+++ CodePaster/CodePaster.sh
@@ -20,10 +20,16 @@ cat temp2.data >> pastedCode.c
 # Trivia fact: -e parameter enables echo command to make the proper interpretation of backslash characters.
 
 echo "$line_separator"
+echo
+cat pastedCode.c
+echo -e "\r\n\r\n"
+
+echo "$line_separator"
 echo "$compilation_msg"
 gcc pastedCode.c -o pastedCode
 rm pastedCode.c
 
 echo "$line_separator"
-echo "$Run_msg"
+echo -e "$Run_msg\n"
 ./pastedCode
+rm pastedCode
