20e6bf7eca208c95bac00fcbcaffc6490c7a4723
JonMS95 <jon_portugalete@hotmail.com>
2022-11-13 18:43:23 +0100

    Added a script pseudo-generator example file.

---
+++ CodePaster/CodePaster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+line_separator="*****************"
+gen_msg="Generating the source file..."
+compilation_msg="Compiling now..."
+Run_msg="Running the program..."
+
+echo "$line_separator"
+echo "Input the text that is meant to be printed."
+read message
+
+echo "$line_separator"
+echo "$gen_msg"
+touch pastedCode.c
+cat temp1.data > pastedCode.c
+echo -e "\n\tconst char* inputStr = \"$message\";" >> pastedCode.c
+cat temp2.data >> pastedCode.c
+
+echo "$line_separator"
+echo "$compilation_msg"
+gcc pastedCode.c -o pastedCode
+rm pastedCode.c
+
+echo "$line_separator"
+echo "$Run_msg"
+./pastedCode
